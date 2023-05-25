12b9258aa514551f193817bef6a3ce699c076d0a
adil-shabab <adilshabab2004@gmail.com>
2023-05-25 17:28:27 +0530

    testimonial section completed

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+
+
+// department carousel 
+if(document.querySelector('.testimonial_carousel') !== null){
+    $('.testimonial_carousel').slick({
+      margin:10,
+      dots: true,
+      infinite: true,
+      // autoplay:true,
+      autoPlayTimeout:2000,
+      slidesToShow: 3,
+      slidesToScroll: 2,
+      arrows: false,
+      responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 900,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+    });
+  }
+  
